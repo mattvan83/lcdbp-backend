02cb8a64d74630adc8d28c094f39508bc64015eb
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-05-17 11:26:59 +0200

    Creation of the backedn of lcdbp

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
+const bodyParser = require("body-parser");
+
+var app = express();
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+
+/* Middlewares */
+app.use(bodyParser.json());
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;

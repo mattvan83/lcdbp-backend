744773b911d8b0f99a7c8f6aeace00d66ccbd44d
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-05-17 12:39:02 +0200

    Create User model

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+require("./models/connection");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
---
+++ models/users.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    voice: {
+      type: String,
+      enum: ["T1", "T2", "B1", "B2"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    incomingDate: {
+      type: Date,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: false,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    token: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;

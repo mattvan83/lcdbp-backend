17bf9988a3286a74543989990201b77c5d8c4311
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-05-23 20:56:07 +0200

    Add gmail authentification with nodemailer

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,48 @@
 var express = require("express");
 var router = express.Router();
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 const Contact = require("../models/contacts");
 const { checkBody } = require("../modules/checkBody");
 
-router.post("/", (req, res) => {
+// Gmail credentials
+const GMAIL_ADDRESS = process.env.GMAIL_ADDRESS;
+const GMAIL_CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+const GMAIL_CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
+const GMAIL_REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+const GMAIL_REDIRECT_URI = process.env.GMAIL_REDIRECT_URI;
+
+const oauth2Client = new OAuth2(
+  GMAIL_CLIENT_ID,
+  GMAIL_CLIENT_SECRET,
+  GMAIL_REDIRECT_URI
+);
+
+oauth2Client.setCredentials({
+  refresh_token: GMAIL_REFRESH_TOKEN,
+});
+
+async function createTransporter() {
+  const accessToken = await oauth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: GMAIL_ADDRESS,
+      clientId: GMAIL_CLIENT_ID,
+      clientSecret: GMAIL_CLIENT_SECRET,
+      refreshToken: GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+
+  return transporter;
+}
+
+router.post("/", async (req, res) => {
   if (
     !checkBody(req.body, [
       "firstname",
@@ -32,18 +70,38 @@ router.post("/", (req, res) => {
     contactFields.phone = phone;
   }
 
-  const newContact = new Contact(contactFields);
+  const emailContent = [
+    `Nom: ${firstname} ${lastname}`,
+    `Email: ${email}`,
+    phone ? `Téléphone: ${phone}` : "",
+    `Message: ${message}`,
+  ]
+    .filter(Boolean)
+    .join("\n");
 
-  newContact.save().then((newContact) => {
-    if (Object.keys(newContact).length) {
-      res.json({ result: true, newContact });
-    } else {
-      res.json({
-        result: false,
-        error: "Error during the saving of the newContact in DB",
-      });
-    }
-  });
+  const mailOptions = {
+    from: GMAIL_ADDRESS,
+    to: GMAIL_ADDRESS,
+    subject: "New Message from Contact Form",
+    text: emailContent,
+  };
+
+  try {
+    const newContact = new Contact(contactFields);
+    await newContact.save();
+
+    const transporter = await createTransporter();
+    // console.log("transporter: ", transporter);
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ result: true, newContact });
+  } catch (error) {
+    res.status(500).json({
+      result: false,
+      error: error,
+    });
+  }
 });
 
 module.exports = router;

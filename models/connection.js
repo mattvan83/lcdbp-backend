53965a670e9c252cb9d8fd1c74c840715fb9e256
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-07-09 20:21:05 +0200

    Remove ignored files from repository

--- models/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectionString = process.env.CONNECTION_STRING;
-
-mongoose
-  .connect(connectionString, { connectTimeoutMS: 5000 })
-  .then(() => console.log("Database connected"))
-  .catch((error) => console.error(error));

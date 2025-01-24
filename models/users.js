e8194058dc0d1eda1cd3fe18c725c082bb090b33
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2025-01-24 17:31:12 +0100

    Set backend to port 3000 and user model unicity based on email field

--- models/users.js
+++ models/users.js
@@ -20,7 +20,7 @@ const userSchema = mongoose.Schema(
     email: {
       type: String,
       required: true,
-      // unique: true,
+      unique: true,
       // match: [
       //   /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
       //   "Please provide a valid email",
